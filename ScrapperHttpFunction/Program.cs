470880a65cc2d140dc0359563a98484b10c5af4d
mykhailo_u <rosmisha@gmail.com>
2025-02-22 22:04:55 +0200

    upd

--- ScrapperHttpFunction/Program.cs
+++ ScrapperHttpFunction/Program.cs
@@ -6,8 +6,6 @@ using ScrapperHttpFunction.Wrappers;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
-builder.ConfigureFunctionsWebApplication();
-
 var connectionString = Environment.GetEnvironmentVariable("LogicAppWorkflowURL");
 if (!string.IsNullOrEmpty(connectionString))
 {
@@ -18,6 +16,8 @@ await container.Initialize();
 
 builder.Services.AddSingleton(container);
 builder.Services.AddTransient<CosmoDbWrapper>();
+
+builder.ConfigureFunctionsWebApplication();
 
 // Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
 // builder.Services
