70eb1183f7de97ed7f9c31387698d904196a84be
mykhailo_u <rosmisha@gmail.com>
2025-03-04 21:48:35 +0200

    communication service

--- ScrapperHttpFunction/Wrappers/LogicAppWrapper.cs
+++ ScrapperHttpFunction/Wrappers/LogicAppWrapper.cs
@@ -17,7 +17,10 @@ public class LogicAppWrapper
     public async Task<bool> CallLogicApp(List<JobInfo> jobList)
     {
         using var client = ClientWrapper.GetInstance();
-        var logicAppUrl = Environment.GetEnvironmentVariable("LogicAppWorkflowURL");
+        
+        // temporary outlook account was suspended so we can try another feature using azure communication service
+        // var logicAppUrl = Environment.GetEnvironmentVariable("LogicAppWorkflowURL"); 
+        var logicAppUrl = Environment.GetEnvironmentVariable("CommunicationLogicApp");
 
         var content = new StringContent(HtmlMessageHelper.BuildHtml(jobList), Encoding.UTF8, "text/html");
 
