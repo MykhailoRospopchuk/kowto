e485a59c7a1446fd6d5b391cedcdd205dc55797b
mykhailo_u <rosmisha@gmail.com>
2025-03-08 23:12:38 +0200

    add report processing

--- ScrapperHttpFunction/CreateReport.cs
+++ ScrapperHttpFunction/CreateReport.cs
@@ -8,12 +8,17 @@ using System.Reflection;
 using System.Text;
 using Common.HtmlResources;
 using CosmoDatabase.Entities;
+using Models.DatabaseModels;
+using Newtonsoft.Json;
 using Wrappers;
 
 public class CreateReport
 {
     private readonly ILogger<CreateReport> _logger;
     private readonly CosmoDbWrapper _cosmoDbWrapper;
+
+    private const string DataPlaceholder = "{{data = [];}}";
+    private const string HtmlTemplate = "ReportTemplate.html";
 
     public CreateReport(ILogger<CreateReport> logger, CosmoDbWrapper cosmoDbWrapper)
     {
@@ -24,27 +29,43 @@ public class CreateReport
     [Function(nameof(CreateReport))]
     public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req)
     {
-        _logger.LogInformation("C# HTTP trigger function processed a request.");
-        
-        var existJobs = await _cosmoDbWrapper.GetRecords<JobInfo, JobInfo>();
-        
-        var assembly = Assembly.GetExecutingAssembly();
-        var resourceName = typeof(HtmlIndexAssembly).Assembly.GetManifestResourceNames().FirstOrDefault(x => x.Contains("ReportTemplate.html"));
-
-        if (string.IsNullOrEmpty(resourceName))
+        try
         {
-            return new BadRequestObjectResult("The report template resource is not available.");
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+        
+            var existJobs = await _cosmoDbWrapper.GetRecords<JobInfo, JobInfoOutModel>();
+            var data = JsonConvert.SerializeObject(existJobs);
+            var replaceText = $"data = {data};";
+        
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = typeof(HtmlIndexAssembly).Assembly.GetManifestResourceNames().FirstOrDefault(x => x.Contains(HtmlTemplate));
+
+            if (string.IsNullOrEmpty(resourceName))
+            {
+                _logger.LogError("The report template resource is not available.");
+                return new BadRequestObjectResult("The report template resource is not available.");
+            }
+
+            string htmlTemplate;
+            using (var stream = assembly.GetManifestResourceStream(resourceName))
+            using (var reader = new StreamReader(stream))
+            {
+                htmlTemplate = await reader.ReadToEndAsync();
+            }
+
+            var htmlBuilder = new StringBuilder(htmlTemplate);
+            htmlBuilder.Replace(DataPlaceholder, replaceText);
+            
+            return new ContentResult()
+            {
+                Content = htmlBuilder.ToString(),
+                ContentType = "text/html",
+            };
         }
-
-        using var stream = assembly.GetManifestResourceStream(resourceName);
-        using var reader = new StreamReader(stream);
-
-        var htmlBuilder = new StringBuilder(await reader.ReadToEndAsync());
-
-        return new ContentResult()
+        catch (Exception e)
         {
-            Content = htmlBuilder.ToString(),
-            ContentType = "text/html",
-        };
+            _logger.LogError(e, "An error occurred while generating the report.");
+            return new BadRequestObjectResult("An error occurred while generating the report.");
+        }
     }
 }
