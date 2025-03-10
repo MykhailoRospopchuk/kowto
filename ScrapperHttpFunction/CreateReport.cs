5fb6e5043925b3334ca28369ac9ab3c56336eba2
mykhailo_l <rosmisha@gmail.com>
2025-03-10 12:39:51 +0200

    upd trigger

--- ScrapperHttpFunction/CreateReport.cs
+++ ScrapperHttpFunction/CreateReport.cs
@@ -33,7 +33,8 @@ public class CreateReport
     }
 
     [Function(nameof(CreateReport))]
-    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req)
+    // public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req)
+    public async Task<IActionResult> Run([TimerTrigger("0 0 5 * * *")] TimerInfo req)
     {
         try
         {
