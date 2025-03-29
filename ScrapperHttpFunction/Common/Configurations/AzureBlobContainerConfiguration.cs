83127a7910be96e2cfb9008dc0b742b103ba90f1
mykhailo_u <rosmisha@gmail.com>
2025-03-29 22:53:31 +0200

    refactor due to  changing blob to private access

--- ScrapperHttpFunction/Common/Configurations/AzureBlobContainerConfiguration.cs
+++ ScrapperHttpFunction/Common/Configurations/AzureBlobContainerConfiguration.cs
@@ -2,6 +2,7 @@ namespace ScrapperHttpFunction.Common.Configurations;
 
 public class AzureBlobContainerConfiguration
 {
-    public string Signature { get; init; }
-    public string ContainerUri { get; init; }
+    public string StorageContainerUrl { get; init; }
+    public string StorageAccountName { get; init; }
+    public string StorageAccountKey { get; init; }
 }
