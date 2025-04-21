993f759ec62009192656a0d63153e59e6f9232ad
mykhailo_l <rosmisha@gmail.com>
2025-04-21 10:38:20 +0300

    upd cron expresion

--- ScrapperHttpFunction/VacancyScrapper.cs
+++ ScrapperHttpFunction/VacancyScrapper.cs
@@ -33,7 +33,7 @@ public class VacancyScrapper
 
     // TODO: change to scheduled trigger
     [Function(nameof(VacancyScrapper))]
-    public async Task<IActionResult> Run([TimerTrigger("0 0 6-22 * * *")] TimerInfo req, CancellationToken cancellationToken)
+    public async Task<IActionResult> Run([TimerTrigger("0 0 6-22/2 * * 1-5")] TimerInfo req, CancellationToken cancellationToken)
     // public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req, CancellationToken cancellationToken)
     {
         _logger.LogInformation("C# HTTP trigger function processed a request. Action");
